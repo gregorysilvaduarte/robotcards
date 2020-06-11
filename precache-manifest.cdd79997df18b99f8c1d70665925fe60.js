8f703234b8b37c8d6eb1a16e4a1d90d510a09271
Gregory Duarte <gregorysilvaduarte@gmail.com>
2020-06-11 00:18:00 -0300

    Updates

---
+++ precache-manifest.cdd79997df18b99f8c1d70665925fe60.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043441d788afcfc5ffc2d3f2fcc51d0a",
+    "url": "/gregorysilvaduarte/robotcards.git/index.html"
+  },
+  {
+    "revision": "78123292ad52b846792c",
+    "url": "/gregorysilvaduarte/robotcards.git/static/css/main.dd7923ef.chunk.css"
+  },
+  {
+    "revision": "03e5d93609138f18853b",
+    "url": "/gregorysilvaduarte/robotcards.git/static/js/2.6626c70c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gregorysilvaduarte/robotcards.git/static/js/2.6626c70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78123292ad52b846792c",
+    "url": "/gregorysilvaduarte/robotcards.git/static/js/main.85752f28.chunk.js"
+  },
+  {
+    "revision": "d8bc8cda5d98e9cd55a5",
+    "url": "/gregorysilvaduarte/robotcards.git/static/js/runtime-main.9f6cd337.js"
+  },
+  {
+    "revision": "7357745905a0c238489fee6097c9662c",
+    "url": "/gregorysilvaduarte/robotcards.git/static/media/ProximaNova-Light-webfont.73577459.woff2"
+  },
+  {
+    "revision": "e382c667a81127659f464991b4013dfd",
+    "url": "/gregorysilvaduarte/robotcards.git/static/media/ProximaNova-Reg-webfont.e382c667.woff2"
+  },
+  {
+    "revision": "2b15976547d0476798183e90246a6c46",
+    "url": "/gregorysilvaduarte/robotcards.git/static/media/ProximaNova-Sbold-webfont.2b159765.woff2"
+  },
+  {
+    "revision": "f158198736da73e229a5fc8d3c26addb",
+    "url": "/gregorysilvaduarte/robotcards.git/static/media/map.f1581987.png"
+  }
+]);
